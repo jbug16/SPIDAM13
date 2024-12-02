87adfa66cf1d0749231e87d84910fdf13a317abb
Jenna Curls <98861047+jbug16@users.noreply.github.com>
2024-12-02 15:15:46 -0500

    Update GUI to fit with model

--- GUI.py
+++ GUI.py
@@ -9,9 +9,10 @@ class AudioApp:
         self.root.title("SPIDAM")
 
         # Set Window Size
-        self.root.geometry("400x300")
+        self.root.geometry("800x600")
 
         # Variables
+        self.file = None
         self.file_name = tk.StringVar(value="No file selected")
         self.duration = tk.StringVar(value="Duration: N/A")
 
@@ -46,20 +47,19 @@ class AudioApp:
             filetypes=[("Audio files", "*.wav"), ("All files", "*.*")]
         )
 
-        # Get duration using wave
+        
         try:
             with wave.open(self.file, 'rb') as wav_file:
+                # Get name
                 self.file_name.set(os.path.basename(self.file))
-        except Exception as e:
-            self.file_name.set("No file selected")
-        # Get duration using wave
-        try:
-            with wave.open(self.file, 'rb') as wav_file:
+
+                # Get duration
                 frames = wav_file.getnframes()
                 rate = wav_file.getframerate()
                 duration = frames / float(rate)
                 self.duration.set(f"Duration: {duration:.2f} seconds")
         except Exception as e:
+            self.file_name.set("No file selected")
             self.duration.set("Duration: N/A")
 
     def combine_plots(self):
